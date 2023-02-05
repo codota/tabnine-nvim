c1e1a3d6902a25d6f25fa4f9f748a2498f73f6c5
Amir Bilu <amirbilu@gmail.com>
2023-02-05 10:43:33 +0200

    bump version

--- lua/tabnine/consts.lua
+++ lua/tabnine/consts.lua
@@ -1,7 +1,7 @@
 local api = vim.api
 
 return {
-	plugin_version = "1.0.0-beta",
+	plugin_version = "1.0.0",
 	max_chars = 3000,
 	tabnine_hl_group = "TabnineSuggestion",
 	tabnine_namespace = api.nvim_create_namespace("tabnine"),
