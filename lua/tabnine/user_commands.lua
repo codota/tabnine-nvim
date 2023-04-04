03e362930737e3d93efddb79923122637dcbad8f
aarondill <aaronsacks2006@gmail.com>
2023-04-04 15:40:40 -0500

    add enable/disable cmds

--- lua/tabnine/user_commands.lua
+++ lua/tabnine/user_commands.lua
@@ -1,16 +1,25 @@
 local M = {}
 local api = vim.api
 local tabnine_binary = require("tabnine.binary")
+local state = require("tabnine.state")
 
 function M.setup()
 	api.nvim_create_user_command("TabnineHub", function()
-		tabnine_binary:request({ Configuration = {} }, function() end)
+		tabnine_binary:request({ Configuration = {} }, function()
+		end)
 	end, {})
 
 	api.nvim_create_user_command("TabnineHubUrl", function()
 		tabnine_binary:request({ Configuration = { quiet = true } }, function(response)
 			print(response.message)
 		end)
+	end, {})
+
+	api.nvim_create_user_command("TabnineEnable", function()
+		state.active = true
+	end, {})
+	api.nvim_create_user_command("TabnineDisable", function()
+		state.active = false
 	end, {})
 end
 
