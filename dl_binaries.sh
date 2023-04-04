d03e75115d3a2cc0bdfb658b77e2e0bf029aea9e
aarondill <aaronsacks2006@gmail.com>
2023-04-04 09:48:21 -0500

    allow passing positional arguments to force specific versions to install

--- dl_binaries.sh
+++ dl_binaries.sh
@@ -4,24 +4,29 @@ set -e
 # This script downloads the binaries for the most recent version of TabNine.
 # Infrastructure detection heavily inspired by https://github.com/tzachar/cmp-tabnine/blob/main/install.sh
 version=${version:-$(curl -sS https://update.tabnine.com/bundles/version)}
-case $(uname -s) in
-"Darwin")
-    if [ "$(uname -m)" = "arm64" ]; then
-        targets="aarch64-apple-darwin"
-    else
-        targets="$(uname -m)-apple-darwin"
-    fi
-    ;;
-"Linux")
-    targets="$(uname -m)-unknown-linux-musl"
-    ;;
-*)
-    echo "Infrastructure detection failed. Installing all versions"
-    targets='x86_64-apple-darwin
+if [ $# -gt 0 ]; then
+    # Pass fully qualified names as positional arguments
+    targets="$(printf '%s\n' "$@")"
+else
+    case $(uname -s) in
+    "Darwin")
+        if [ "$(uname -m)" = "arm64" ]; then
+            targets="aarch64-apple-darwin"
+        else
+            targets="$(uname -m)-apple-darwin"
+        fi
+        ;;
+    "Linux")
+        targets="$(uname -m)-unknown-linux-musl"
+        ;;
+    *)
+        echo "Infrastructure detection failed. Installing all versions"
+        targets='x86_64-apple-darwin
     x86_64-unknown-linux-musl
     aarch64-apple-darwin'
-    ;;
-esac
+        ;;
+    esac
+fi
 
 rm -rf ./binaries
 
@@ -29,7 +34,8 @@ echo "$targets" | while read -r target; do
     mkdir -p "binaries/$version/$target"
     path=$version/$target
     echo "downloading $path"
-    curl -sS "https://update.tabnine.com/bundles/$path/TabNine.zip" >"binaries/$path/TabNine.zip"
+    curl -fsS "https://update.tabnine.com/bundles/$path/TabNine.zip" -o "binaries/$path/TabNine.zip" ||
+        continue
     unzip -o "binaries/$path/TabNine.zip" -d "binaries/$path"
     rm "binaries/$path/TabNine.zip"
     chmod +x "binaries/$path/"*
