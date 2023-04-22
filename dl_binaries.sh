e44b03735d840a56fc01a9ed0e23a3dd93848449
aarondill <aaronsacks2006@gmail.com>
2023-04-22 13:07:53 -0500

    Check if dependencies are available.
    If not, print message for each missing dependency and exit.

--- dl_binaries.sh
+++ dl_binaries.sh
@@ -1,32 +1,44 @@
 #!/bin/sh
 set -e
 
+DEPENDS='unzip curl' # list of dependencies (commands)
+HAS_ALL_DEPS=1       # 1 = present, 0 = missing
+for dep in ${DEPENDS}; do
+  if ! command -v "$dep" >/dev/null 2>/dev/null; then # if command not accessible
+    echo "ERROR: $dep is required to download Tabnine binaries. Please install $dep and run this again." >&2
+    HAS_ALL_DEPS=0
+  fi
+done
+if [ "${HAS_ALL_DEPS}" -eq 0 ]; then # missing something.
+  exit 1
+fi
+
 # This script downloads the binaries for the most recent version of TabNine.
 # Infrastructure detection heavily inspired by https://github.com/tzachar/cmp-tabnine/blob/main/install.sh
 version=${version:-$(curl -sS https://update.tabnine.com/bundles/version)}
 if [ $# -gt 0 ]; then
-    # Pass fully qualified names as positional arguments
-    targets="$(printf '%s\n' "$@")"
+  # Pass fully qualified names as positional arguments
+  targets="$(printf '%s\n' "$@")"
 else
-    case $(uname -s) in
-    "Darwin")
-        if [ "$(uname -m)" = "arm64" ]; then
-            targets="aarch64-apple-darwin"
-        elif [ "$(uname -m)" = "x86_64" ]; then
-            targets="x86_64-apple-darwin"
-        fi
-        ;;
-    "Linux")
-        if [ "$(uname -m)" = "x86_64" ]; then
-            targets="x86_64-unknown-linux-musl"
-        fi
-        ;;
-    esac
+  case $(uname -s) in
+  "Darwin")
+    if [ "$(uname -m)" = "arm64" ]; then
+      targets="aarch64-apple-darwin"
+    elif [ "$(uname -m)" = "x86_64" ]; then
+      targets="x86_64-apple-darwin"
+    fi
+    ;;
+  "Linux")
+    if [ "$(uname -m)" = "x86_64" ]; then
+      targets="x86_64-unknown-linux-musl"
+    fi
+    ;;
+  esac
 fi
 
 if [ -z "$targets" ]; then
-    echo "Infrastructure detection failed. Installing all versions"
-    targets='x86_64-apple-darwin
+  echo "Infrastructure detection failed. Installing all versions"
+  targets='x86_64-apple-darwin
     x86_64-unknown-linux-musl
     aarch64-apple-darwin'
 fi
@@ -34,12 +46,12 @@ fi
 rm -rf ./binaries
 
 echo "$targets" | while read -r target; do
-    mkdir -p "binaries/$version/$target"
-    path=$version/$target
-    echo "downloading $path"
-    curl -fsS "https://update.tabnine.com/bundles/$path/TabNine.zip" -o "binaries/$path/TabNine.zip" ||
-        continue
-    unzip -o "binaries/$path/TabNine.zip" -d "binaries/$path"
-    rm "binaries/$path/TabNine.zip"
-    chmod +x "binaries/$path/"*
+  mkdir -p "binaries/$version/$target"
+  path=$version/$target
+  echo "downloading $path"
+  curl -fsS "https://update.tabnine.com/bundles/$path/TabNine.zip" -o "binaries/$path/TabNine.zip" ||
+    continue
+  unzip -o "binaries/$path/TabNine.zip" -d "binaries/$path"
+  rm "binaries/$path/TabNine.zip"
+  chmod +x "binaries/$path/"*
 done
