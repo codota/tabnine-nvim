e3723a68234a287fcc42515b891869d1f3e38af2
Aaron Dill <117116764+aarondill@users.noreply.github.com>
2023-04-23 23:34:16 -0500

    [Feature] Avoid download of *all* binaries on install. (#53)
    * only dl binaries for current infrastructure
    
    * allow passing positional arguments to force specific versions to install
    
    * add autodetection and args to dl_binaries.ps1
    
    * continue loop on failure
    
    * fix architecture detection due to no support on linux arm chips
    
    * Check if dependencies are available.
    
    If not, print message for each missing dependency and exit.
    
    * Expand a few PowerShell aliases.
    
    Aliases are *slightly* less portable and readable than the expanded
    version, and best practice is to use the expanded version in scripts
    rather than aliases.
    
    * Update dl_binaries.sh
    
    Co-authored-by: Amir Bilu <amirbilu@gmail.com>
    
    ---------
    
    Co-authored-by: Amir Bilu <amirbilu@gmail.com>

--- dl_binaries.ps1
+++ dl_binaries.ps1
@@ -3,24 +3,37 @@
 # This script downloads the binaries for the most recent version of TabNine.
 
 $version = invoke-webrequest -uri 'https://update.tabnine.com/bundles/version' -usebasicparsing
-$targets = @(
-    'i686-pc-windows-gnu'
-    'x86_64-pc-windows-gnu'
-)
+if ($args.count -ne 0) {
+    $targets = $args
+}
+else {
+    if ((Get-WmiObject win32_operatingsystem | Select-Object osarchitecture).osarchitecture -eq "64-bit") {
+        $targets = @(
+            'x86_64-pc-windows-gnu'
+        )
+    }
+    else {
+        $targets = @(
+            'i686-pc-windows-gnu'
+        )
+    }
+}
 
 if (test-path -path "binaries/$version") {
     remove-item -path binaries -recurse -force | out-null
-    mkdir binaries/$version -force | out-null
+    New-Item -Path "binaries/$version" -ItemType Directory -force | out-null
 }
 
 $targets | foreach-object {
     $target = $_
     $path = "$version/$target"
 
-    if (!(test-path -path "binaries/$version/$target")) { mkdir "binaries/$version/$target" -force | out-null }
+    if (!(test-path -path "binaries/$version/$target")) { New-Item -Path "binaries/$version/$target" -ItemType Directory -force | out-null }
 
-    echo "downloading $path"    
+    Write-Output "downloading $path"    
     invoke-webrequest -uri "https://update.tabnine.com/bundles/$path/TabNine.zip" -outfile "binaries/$path/TabNine.zip"
+    # Stop this iteration if the download failed
+    if ($LastExitCode -eq 0) {return}
 
     expand-archive "binaries/$path/TabNine.zip" "binaries/$path" 
 
--- dl_binaries.sh
+++ dl_binaries.sh
@@ -1,22 +1,57 @@
 #!/bin/sh
 set -e
 
-# This script downloads the binaries for the most recent version of TabNine.
+DEPENDS='unzip curl' # list of dependencies (commands)
+HAS_ALL_DEPS=1       # 1 = present, 0 = missing
+for dep in ${DEPENDS}; do
+  if ! command -v "$dep" >/dev/null 2>/dev/null; then # if command not accessible
+    echo "ERROR: $dep is required to download Tabnine binaries. Please install $dep and run this again." >&2
+    HAS_ALL_DEPS=0
+  fi
+done
+if [ "${HAS_ALL_DEPS}" -eq 0 ]; then # missing something.
+  exit 1
+fi
 
-version="$(curl -sS https://update.tabnine.com/bundles/version)"
-targets='x86_64-apple-darwin
+# This script downloads the binaries for the most recent version of TabNine.
+# Infrastructure detection heavily inspired by https://github.com/tzachar/cmp-tabnine/blob/main/install.sh
+version=${version:-$(curl -sS https://update.tabnine.com/bundles/version)}
+if [ $# -gt 0 ]; then
+  # Pass fully qualified names as positional arguments
+  targets="$(printf '%s\n' "$@")"
+else
+  case $(uname -s) in
+  "Darwin")
+    if [ "$(uname -m)" = "arm64" ]; then
+      targets="aarch64-apple-darwin"
+    elif [ "$(uname -m)" = "x86_64" ]; then
+      targets="x86_64-apple-darwin"
+    fi
+    ;;
+  "Linux")
+    if [ "$(uname -m)" = "x86_64" ]; then
+      targets="x86_64-unknown-linux-musl"
+    fi
+    ;;
+  esac
+fi
+
+if [ -z "$targets" ]; then
+  echo "Target detection failed. Installing all targets"
+  targets='x86_64-apple-darwin
     x86_64-unknown-linux-musl
     aarch64-apple-darwin'
+fi
 
 rm -rf ./binaries
 
-echo "$targets" | while read target
-do
-    mkdir -p binaries/$version/$target
-    path=$version/$target
-    echo "downloading $path"
-    curl -sS https://update.tabnine.com/bundles/$path/TabNine.zip > binaries/$path/TabNine.zip
-    unzip -o binaries/$path/TabNine.zip -d binaries/$path
-    rm binaries/$path/TabNine.zip
-    chmod +x binaries/$path/*
+echo "$targets" | while read -r target; do
+  mkdir -p "binaries/$version/$target"
+  path=$version/$target
+  echo "downloading $path"
+  curl -fsS "https://update.tabnine.com/bundles/$path/TabNine.zip" -o "binaries/$path/TabNine.zip" ||
+    continue
+  unzip -o "binaries/$path/TabNine.zip" -d "binaries/$path"
+  rm "binaries/$path/TabNine.zip"
+  chmod +x "binaries/$path/"*
 done
