bf56dbda6486fa9129737040123131ed96a61b8b
aarondill <aaronsacks2006@gmail.com>
2023-06-17 03:39:22 -0500

    move min_version to tabnine.consts.min_nvim_version

--- lua/tabnine.lua
+++ lua/tabnine.lua
@@ -1,4 +1,5 @@
 local config = require("tabnine.config")
+local consts = require("tabnine.consts")
 local auto_commands = require("tabnine.auto_commands")
 local user_commands = require("tabnine.user_commands")
 local status = require("tabnine.status")
@@ -9,13 +10,12 @@ local M = {}
 function M.setup(o)
 	config.set_config(o)
 
-	local min_version = "0.7.1"
 	local v = vim.version()
-	if vim.version.lt(v, vim.version.parse(min_version) or {}) then
+	if vim.version.lt(v, vim.version.parse(consts.min_nvim_version) or {}) then
 		vim.notify_once(
 			string.format(
 				"tabnine-nvim requires neovim version >=%s. Current version: %d.%d.%d",
-				min_version,
+				consts.min_nvim_version,
 				v.major,
 				v.minor,
 				v.patch
--- lua/tabnine/consts.lua
+++ lua/tabnine/consts.lua
@@ -2,6 +2,7 @@ local api = vim.api
 
 return {
 	plugin_version = "1.4.0",
+	min_nvim_version = "0.7.1",
 	max_chars = 3000,
 	tabnine_hl_group = "TabnineSuggestion",
 	tabnine_namespace = api.nvim_create_namespace("tabnine"),
