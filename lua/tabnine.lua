116348a92fad7137c86ad3032c98abbcb991e505
Aaron Dill <117116764+aarondill@users.noreply.github.com>
2023-06-18 01:12:18 -0500

    Notify the user if the nvim version is too low to use tabnine-nvim (#79)
    * notify if version is too low
    
    * Create min_version variable for easy changing later
    
    If necessary.
    
    * vim.cmd.version doesn't return a string!
    
    This caused a nil contatenation error. This is fixed by constructing our own string with vim.version.*
    
    * move min_version to tabnine.consts.min_nvim_version

--- lua/tabnine.lua
+++ lua/tabnine.lua
@@ -1,4 +1,5 @@
 local config = require("tabnine.config")
+local consts = require("tabnine.consts")
 local auto_commands = require("tabnine.auto_commands")
 local user_commands = require("tabnine.user_commands")
 local status = require("tabnine.status")
@@ -8,6 +9,21 @@ local M = {}
 
 function M.setup(o)
 	config.set_config(o)
+
+	local v = vim.version()
+	if vim.version.lt(v, vim.version.parse(consts.min_nvim_version) or {}) then
+		vim.notify_once(
+			string.format(
+				"tabnine-nvim requires neovim version >=%s. Current version: %d.%d.%d",
+				consts.min_nvim_version,
+				v.major,
+				v.minor,
+				v.patch
+			),
+			vim.log.levels.WARN
+		)
+		return nil
+	end
 
 	keymaps.setup()
 
