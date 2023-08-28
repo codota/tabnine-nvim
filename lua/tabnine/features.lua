dcca6d2f9e8bbcde36aab87b9b87890810e4056d
Amir Bilu <amirbilu@gmail.com>
2023-08-28 15:08:13 +0300

    Update chat version (#110)

--- lua/tabnine/features.lua
+++ lua/tabnine/features.lua
@@ -3,8 +3,10 @@ local tabnine_binary = require("tabnine.binary")
 
 function M.if_feature_enabled(features, run_if_enabled)
 	tabnine_binary:request({ Features = { dummy = true } }, function(response)
-	if not response or not response.enabled_features then return end
-    for _, feature in ipairs(features) do
+		if not response or not response.enabled_features then
+			return
+		end
+		for _, feature in ipairs(features) do
 			if vim.tbl_contains(response.enabled_features, feature) then
 				run_if_enabled()
 				return
