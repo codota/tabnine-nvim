ff843ad4e86c850551413b5bc623fee14f8fd239
Amir Bilu <amirbilu@gmail.com>
2023-09-20 09:56:37 +0300

    prefetch

--- lua/tabnine/config.lua
+++ lua/tabnine/config.lua
@@ -8,7 +8,7 @@ function M.set_config(o)
 		dismiss_keymap = "<C-]>",
 		debounce_ms = 800,
 		suggestion_color = { gui = "#808080", cterm = 244 },
-		exclude_filetypes = { "TelescopePrompt" },
+		exclude_filetypes = { "TelescopePrompt", "NvimTree" },
 		log_file_path = nil,
 		tabnine_enterprise_host = nil,
 	}, o or {})
