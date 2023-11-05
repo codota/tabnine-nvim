1073bdd7a20cc00aba60025207b50cec5d385a8e
aarondill <aaronsacks2006@gmail.com>
2023-11-05 16:06:39 -0600

    fix: add pipe for stderr to show the user errors when they occur.
    We don't care about stdout since it's just progress indicators. These are too noisy to notify on.

--- build.lua
+++ build.lua
@@ -8,7 +8,12 @@ else
 	args = {}
 end
 
-local handle, pid = uv.spawn(cmd, { args = args }, function(code)
+local stderr = assert(uv.new_pipe())
+
+local handle, pid = uv.spawn(cmd, {
+	args = args,
+	stdio = { nil, nil, stderr },
+}, function(code)
 	if code == 0 then
 		return vim.notify("tabnine-nvim build script finished successfully", vim.log.levels.INFO)
 	else
@@ -21,3 +26,11 @@ if not handle then
 	vim.notify(("Could not spawn tabnine-nvim build script: '%s'. Error: %s"):format(cmd, err), vim.log.levels.WARN)
 	return
 end
+
+uv.read_start(stderr, function(err, data)
+	assert(not err, err)
+	if not data then return end
+	data = data:gsub("%s+$", ""):gsub("^%s+", "") -- remove trailing and leading whitespace
+	if data == "" then return end
+	return vim.notify(("%s: ERROR: %s"):format(cmd, data), vim.log.levels.WARN)
+end)
