b9fea3458693d5f0e43033011001316b3b405c1c
aarondill <aaronsacks2006@gmail.com>
2024-01-16 01:11:14 -0600

    feat: add readme for /tests directory
    This clarifies usage of the tests directory and plenary/busted's interface

--- tests/init_spec.lua
+++ tests/init_spec.lua
@@ -1,13 +1,3 @@
---- See the following links for examples
--- https://github.com/nvim-lua/plenary.nvim?tab=readme-ov-file#plenarytest_harness
--- https://github.com/nvim-lua/plenary.nvim/blob/master/TESTS_README.md
--- https://github.com/lunarmodules/luassert
--- https://github.com/lunarmodules/busted
----- Note: not all busted examples will work. plenary implements a busted-*like* interface.
-
---- Some useful assert functions:
---- is_true, is_false, is_nil, is_boolean, is_number, is_string, is_table, is_function, is_userdata, is_thread
---- ref, same, matches, matches, near, equals, equals, unique, truthy, falsy
 local busted = require("plenary.busted")
 local describe, it = busted.describe, busted.it
 local assert = require("luassert")
