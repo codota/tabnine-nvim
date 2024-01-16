a7e64c0ef7e2b60497bc8c0a29a80d98fc110107
aarondill <aaronsacks2006@gmail.com>
2024-01-16 01:13:45 -0600

    chore: remove placeholder test in init_spec
    replace with a TODO comment

--- tests/init_spec.lua
+++ tests/init_spec.lua
@@ -1,9 +1,1 @@
-local busted = require("plenary.busted")
-local describe, it = busted.describe, busted.it
-local assert = require("luassert")
-
-describe("testing framework", function()
-	it("should run tests", function()
-		assert.truthy(true)
-	end)
-end)
+---TODO: Add tests for the main modules here
