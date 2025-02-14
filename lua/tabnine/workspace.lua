12c6b02c706285a94d06525aa0e7d7c84c89d34d
Amir Bilu <amirbilu@gmail.com>
2025-02-14 16:48:07 +0200

    release

--- lua/tabnine/workspace.lua
+++ lua/tabnine/workspace.lua
@@ -7,28 +7,28 @@ local utils = require("tabnine.utils")
 local M = {}
 
 local function workspace_folders()
-  local config = config.get_config().workspace_folders
-  local result = {}
+	local config = config.get_config().workspace_folders
+	local result = {}
 
-  if config.lsp and #utils.buf_get_clients() > 0 then
-    vim.list_extend(result, utils.set(lsp.buf.list_workspace_folders()))
-  end
+	if config.lsp and #utils.buf_get_clients() > 0 then
+		vim.list_extend(result, utils.set(lsp.buf.list_workspace_folders()))
+	end
 
-  if config.paths then vim.list_extend(result, config.paths) end
+	if config.paths then vim.list_extend(result, config.paths) end
 
-  if config.get_paths then vim.list_extend(result, config.get_paths() or {}) end
+	if config.get_paths then vim.list_extend(result, config.get_paths() or {}) end
 
-  return result
+	return result
 end
 
 function M.update()
-  tabnine_binary:request({ Workspace = { root_paths = workspace_folders() } }, function() end)
+	tabnine_binary:request({ Workspace = { root_paths = workspace_folders() } }, function() end)
 end
 
 function M.setup()
-  local timer = uv.new_timer()
+	local timer = uv.new_timer()
 
-  timer:start(0, 30000, vim.schedule_wrap(M.update))
+	timer:start(0, 30000, vim.schedule_wrap(M.update))
 end
 
 return M
